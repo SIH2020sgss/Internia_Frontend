3ec204821d0e81503906d070280a49c22dfb1474
AtharvaKitkaru <54904845+AtharvaKitkaru@users.noreply.github.com>
2020-07-29 16:43:37 +0530

    plan

--- src/FacultyApp.js
+++ src/FacultyApp.js
@@ -4,14 +4,14 @@ import CreateInternship from "./faculty_components/CreateInternship";
 import { Route } from "react-router-dom";
 import Feedback from "./faculty_components/Feedback";
 import Floatedinternship from "./faculty_components/Floatedinternship";
+import Homepage from "./faculty_components/Homepage";
 
 export default class FacultyApp extends React.Component {
   render() {
     return (
       <div className='FacultyApp min-vh-100'>
         <Header />
-        <Route exact path='/' render={() => "Landing page"} />
-        {/* <Route exact path='/' component={SearchInternships} /> */}
+        <Route exact path='/' component={Homepage} />
         <Route path='/create-internship' component={CreateInternship} />
         <Route path='/feedback' component={Feedback} />
         <Route path='/floated' component={Floatedinternship} />
